84f3e0f8ef924ea540124efe7d784de21d2f0621
Chris Thomas <christopher.thomas@plista.com>
2019-04-24 14:41:06 +0200

    the variable is DOVECOT_LMTP and not DOVECOT_PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ for file in $(find /etc/postfix -type f); do
     required DATABASE_PORT ${file}
 
     required DOVECOT_HOSTNAME ${file}
-    required DOVECOT_PORT ${file}
+    required DOVECOT_LMTP ${file}
 
     required OPENDKIM_HOSTNAME ${file}
     required OPENDKIM_PORT ${file}
